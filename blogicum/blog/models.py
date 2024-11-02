421575b4a0a278c9b21ce152f0c23513bb387d7a
Alexander Ivanov <ivanov281202@gmail.com>
2024-11-02 21:17:37 +0700

    Second try

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -28,7 +28,8 @@ class Post(BaseModel):
     )
     pub_date = models.DateTimeField(
         verbose_name='Дата и время публикации',
-        help_text='Если установить дату и время в будущем — можно делать отложенные публикации.'
+        help_text=('Если установить дату и время в будущем '
+                   '— можно делать отложенные публикации.')
     )
     author = models.ForeignKey(
         User,
@@ -64,7 +65,8 @@ class Category(BaseModel):
     slug = models.SlugField(
         unique=True,
         verbose_name='Идентификатор',
-        help_text='Идентификатор страницы для URL; разрешены символы латиницы, цифры, дефис и подчёркивание.'
+        help_text=('Идентификатор страницы для URL; '
+                   'разрешены символы латиницы, цифры, дефис и подчёркивание.')
     )
     description = models.TextField(
         verbose_name='Описание'
